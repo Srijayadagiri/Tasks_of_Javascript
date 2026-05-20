aaef5189eda0dc674437e09455751eb4492db066
Srijayadagiri <annamanenisrija@gmail.com>
2026-05-20 11:18:32 +0530

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,195 @@
+//1.Create a variable using var and store your name.
+    var Myname= "Srija";
+  
+//2.Print your name using console.log().
+    console.log(Myname);
+
+//3.Create a variable using let and store your age.
+    let Myage= 25;
+
+//4. Change the value of age and print it.
+    Myage= 26;
+    console.log(Myage);
+
+//5.Create a constant variable for country name.
+    const countryname="India";
+
+//6.Print the Country name.
+    console.log(countryname);
+
+//7.Try redeclaring var variable and observe output.
+     var name= "Srija";
+         name= "Yadagiri";
+     var name="Srija Yadagiri"
+    console.log(name);
+
+//8.Try redeclaring let variable and observe error
+     let age=25;
+         age=26;
+     //let age=30; 
+     console.log(age);
+
+//9.Try reassigning const variable and observe error.
+     const Countryname="India";
+           //Countryname="USA";
+     console.log(Countryname);
+
+//10.Write difference between var and let.
+//Var- "var is a keyword used to declare global scope and functional scope variables  that allows redeclaration." 
+//Let- "let is a keyword used to declare block scope variables it doesnot allows redeclaration."
+
+//11.Write difference between let and const
+//Let- "let is a keyword used to decalre block scope variables and it allows to reassign a value to a variable."
+//Const- "Const is a keyword used to declare block scope variables and it does not allows reassign a value it sticks to the intial value."
+
+//12.Predict Output
+var a=10 
+    a=20
+var a=30
+console.log(a)
+output: 30
+
+//13.Predict output
+let b=50
+    b=60
+console.log(b)
+  output:60
+
+//14.Predict output
+const c=100
+console.log(c)
+output: 100
+
+//15.Use console.log() to print "Hello world."
+console.log("Hello World");
+
+//16.Use alert() to show "Welcome User."
+alert("Welcome user");
+
+//17.Use conform() to ask user:
+confirm("Do you like JavaScript?");
+
+//18.Use prompt() to ask user name:
+prompt("User Name");
+
+//19. Print prompt value in concole.
+    let value=prompt("enter a value:");
+    console.log(value);
+
+//20. Use document.writeIn() to print "Batch 33."
+  document.write("batch 33");
+
+//21. Create variable studentName and store your name.
+   let studentName="Srija";
+
+//22.Create variable studentAge and store your age.
+  let studentAge= "23";
+
+//23.Print both varibles together.
+console.log(studentName, studentAge);
+
+//24.Create a variable for mobile number.
+     let mobilenumber="9876543210"
+
+//25.Print mobile number using console.log().
+    console.log(mobilenumber);
+
+//26.Create variable using var for favorite color.
+//27.Reassign another color value.
+   var Fav_color="Blue";
+       Fav_color="Pink";
+    console.log(Fav_color);
+
+//28.Create variable using let for marks.
+//29.Change marks value and print it.
+let marks="75";
+    marks="90"
+console.log(marks);
+
+//30.Create const variable for PI value.
+//31.Print PI value.
+const PIvalue="5.5";
+console.log(PIvalue);
+
+//32.Predict output.
+ var x=5
+ var x=15
+ console.log(x)
+ ouput: 15
+
+//33.predict output
+let y=25
+    y=35
+console.log(y)
+output: 35
+
+//34.Find error
+const z=40
+      //z=45
+console.log(z)
+output:TypeError
+
+//35.Create alert box with your college name.
+alert("Vaageswari college of Engineering");
+
+//36.Ask user age using prompt.
+//37.print user age in console.
+ let Mage=prompt("Enter your age");
+console.log(Mage);
+
+//38.Ask user conformation.
+//did you complete HTML?.
+//39.Print conform result in console.
+let usercompletion="Did you complete HTML?"
+console.log(usercompletion);
+
+//40.print "JavaScript Class using document.writeIn()"
+document.write("Javascript Class");
+
+//41.Create 3 variables:city state country.
+const city="Hyderabad";
+const state="Telangana";
+const country="India";
+
+//42.Print all variables todether.
+console.log(city,state,country);
+
+//43.Create one example for var.
+var Number="125";
+    Number="250";
+var Number="500";
+console.log(Number);
+
+//44.Create one example for let.
+let Name="varalaxmi";
+    Name="Laxmi";
+console.log(Name);
+
+//45.Create one example for const.
+const DOB= "123/03/2003";
+console.log(DOB);
+
+//46.predict output
+var num="100";
+    num="200";
+    console.log(num);
+output: 200
+
+//47.Predict Output 
+let price="500";
+    price="700";
+console.log(price);
+output: 700
+
+//predict output
+const password="12345";
+console.log(password);
+output: 12345
+
+//49.Write rules for var.
+//-Var can be declare in gloabal scope.
+//-Var can be used as redeclaration for the variable.
+
+//50.Write rules of let and const.
+//-Both can be declare variables in block Scope.
+//-Let can be reassign the value where as Const cannot reassign the value.
