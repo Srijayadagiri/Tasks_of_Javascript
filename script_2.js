ad3c4daa057e56728a078d39e87c73a858e5bef4
Srijayadagiri <annamanenisrija@gmail.com>
2026-05-20 13:25:13 +0530

    Add files via upload

---
+++ script_2.js
@@ -0,0 +1,164 @@
+//1.Create a variable and store your favourite food.
+var Fav_food= "Sambar Rice";
+console.log(Fav_food);
+
+//2.Create a variable and store your mobile number.
+var MobileNumber= "1234598760";
+console.log(MobileNumber);
+
+//3.Create a boolean variable with false value
+let a= "false";
+console.log(a);
+
+//4.Create a undefined variable and print typeof.
+let a1="Hyderabad";
+console.log(typeof(a1));
+let a2=12345;
+console.log(typeof(a2));
+
+//5.Create one array with 6 animal names.
+ let animal_names=["Elephant","Lion","Dog","giraffe","Monkey","Cow"];
+ console.log(animal_names);
+
+//6.Print Second animal from array.
+console.log(animal_names[1]);
+
+//7.Print last animal array using length.
+console.log(animal_names[animal_names.length-1])
+//8.Create object with bike details(name,model,color).
+let bike_details={
+    name:"Hero Honda",
+    model:"Splender Plus",
+    color:"green and black",
+}
+console.log(bike_details);
+//9.Print bike color from object.
+console.log(bike_details.color);
+
+//10.Create two variables and subtract  values.
+let v1=125;
+let v2=50;
+console.log(v1-v2);
+
+//11.Create two variables and divide values.
+let v3=126;
+let v4=6;
+console.log(v3/v4);
+
+//12.Find 15%2.
+console.log(15%2);
+
+//13.Find 2**5.
+console.log(2**5);
+
+//14.Create  variable with 5 and use post increment.
+let x1=5;
+console.log(x1++);
+
+//15.Create variable with 10 and use pre increment.
+let x2=10;
+console.log(++x2);
+
+//16.create variable with 20 and use post decrement.
+let X3=20;
+console.log(X3--);
+
+//17.Cretae variable with 50 and use pre decfrement.
+let x4=50;
+console.log(--x4);
+
+//18.check 100>50.
+console.log(100 > 50);
+
+//19.Check 25<10.
+console.log(25 < 10);
+
+//20.check 50>=50.
+console.log(50 >= 50);
+
+//21.Check 30<=25.
+console.log(30 <= 25);
+
+//22.Check 10=="10".
+console.log(10 == "10");
+
+//23.Check 10==="10".
+console.log(10 === "10");
+
+//24.Check 25!="25".
+console.log(25 != "25");
+
+//25.Check 25!=="25".
+console.log(25 !== "25");
+
+//26.Use AND operato:
+//10>5&&20>15
+console.log(10>5 && 20>15);
+
+//27.Use And operator:
+//5>10&&20>15
+console.log(5>10 && 20>15);
+
+//28.Use OR operator:
+//5>10||20>15
+console.log(5>10 || 20>15);
+
+//29.Use OR opertaor.
+//2>5||1>10
+console.log(2>5 || 1>10);
+
+//30.Use NOT operator:
+//!(10>5)
+console.log(!(10 > 5));
+
+//31.Use NOT operator:
+//!(5>10)
+console.log(!(5 > 10));
+
+//32.Create Ternary operator:
+//if 50>25 print "True"
+//else print "False".
+let z1= 50 > 25 ? "True" : "False";
+console.log(z1);
+
+//33.Create Ternary opeartor:
+//if 18>=21 print "Adult"
+//else print"Minor".
+let z2= 18>= 21 ? "Adult" : "minor";
+console.log(z2);
+
+//34.Create one array with 5 city names and print third city.
+let city_names=["Hyderabad","Bangolre","chennai","Mumbai","Kolkata"];
+console.log(city_names[2]);
+
+//35.Create one object with employee details and Print employee name.
+let emp_details={
+    name: "Siri",
+    Designation : "Software Engineer",
+    Salary: 40000
+}
+console.log(emp_details.name);
+
+//36.Create variable with string "100" and print typeof.
+let str= "100";
+console.log(typeof(str));
+
+//37.Create variable with number 100 and print typeof.
+let num=100;
+console.log(typeof(num));
+
+//38.Create one null variable and print it.
+let var1="null";
+console.log(var1);
+
+//39.Create one array with mixed datatypes.
+let Mixed_DT=["Apple",100,true,null,["1,2,3"]];
+console.log(Mixed_DT);
+
+//40.Create object with student details name,mark,passed(boolean).
+let Stu_details={
+    name:"Raju",
+    mark:64,
+    passed:true
+}
+console.log(Stu_details);
